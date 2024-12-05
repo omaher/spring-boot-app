bc785d3a2c6a07057d625b85a33d1209fb36cd57
Omkar Aher <aher.omkar.15et7021@gmail.com>
2024-12-06 00:45:10 +0530

    Added docker tool for tls cert verification error

--- jenkins/dev-pipeline.groovy
+++ jenkins/dev-pipeline.groovy
@@ -3,7 +3,7 @@ pipeline {
 
     tools {
         maven 'maven-3.8.6' // Match the name from Global Tool Configuration
-        docker 'docker' // Match the docker tool name from Tool Configuration
+        docker 'docker-latest' // Match the docker tool name from Tool Configuration
     }
 
     environment {
